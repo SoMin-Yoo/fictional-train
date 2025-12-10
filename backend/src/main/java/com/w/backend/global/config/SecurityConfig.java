59bade51a2d67d08bfa6abbb7a68d2ad2daa8c3f
Yoo Seung-jun <funczun@gmail.com>
2025-12-10 12:52:16 +0900

    chore(security): H2 콘솔 체크를 위해 sameOrigin 프레임 허용

--- backend/src/main/java/com/w/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/w/backend/global/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,8 +17,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
         http
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
             .authorizeHttpRequests(
-                auth -> auth.requestMatchers("/", "/error").permitAll().anyRequest()
+                auth -> auth.requestMatchers("/users", "/error").permitAll().anyRequest()
                     .authenticated())
             .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
             .logout(LogoutConfigurer::permitAll);
