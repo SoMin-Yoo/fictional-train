059b0a161783c147c2f8361a66da05ea7ce9063f
Yoo Seung-jun <funczun@gmail.com>
2025-12-12 04:38:20 +0900

    feat(login): 로그인 뷰 구현

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
+import '@/asset/main.css'
 
 import App from './App.vue'
 import router from './router'
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import UserCreate from "@/components/user/UserCreate.vue";
 import HomeView from "@/views/HomeView.vue";
-import UserView from "@/views/UserView.vue";
-import UserLogin from "@/components/user/UserLogin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,23 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: HomeView,
-    },
-    {
-      path: '/user',
-      component: UserView,
-      children: [
-        {
-          path: 'regist',
-          name: 'Regist',
-          component: UserCreate,
-        },
-        {
-          path: 'login',
-          name: 'Login',
-          component: UserLogin,
-        },
-      ],
-    },
+    }
   ],
 });
 
